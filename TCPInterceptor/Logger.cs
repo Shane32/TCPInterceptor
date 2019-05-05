8cfe89f39e294d61e4705f602b4b1babc1491107
Shane Krueger <shane@acdmail.com>
2019-05-05 17:21:23 -0400

    Added TCPListener

---
+++ TCPInterceptor/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPInterceptor
+{
+    class Logger
+    {
+        public DelayedSemaphore GetDelayedSemaphore()
+        {
+            return null;
+        }
+
+        public async Task Log(int port, bool outbound, byte[] buffer, int offset, int count) {
+
+        }
+
+    }
+}
--- TCPInterceptor/TCPListener.cs
+++ TCPInterceptor/TCPListener.cs
@@ -1,12 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TCPInterceptor
 {
     class TCPListener
     {
+        private readonly int _port;
+        private readonly IPAddress _target;
+        private readonly Logger _logger;
+
+        public TCPListener(int port, IPAddress target, Logger logger)
+        {
+            _port = port == 0 ? throw new ArgumentOutOfRangeException(nameof(port)) : port;
+            _target = target ?? throw new ArgumentOutOfRangeException(nameof(target));
+            _logger = logger;
+        }
+
+        public async void Start(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested) return;
+            System.Net.Sockets.TcpListener tcpListener = new System.Net.Sockets.TcpListener(IPAddress.Any, _port);
+            tcpListener.Start();
+            cancellationToken.Register(() =>
+            {
+                tcpListener.Stop();
+            });
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                TcpClient tcpClient = null;
+                try
+                {
+                    tcpClient = await tcpListener.AcceptTcpClientAsync();
+                }
+                catch (ObjectDisposedException)
+                {
+                    break;
+                }
+                StartClient(cancellationToken, tcpClient);
+            }
+
+        }
+
+        private async void StartClient(CancellationToken cancellationToken, TcpClient tcpClient)
+        {
+            cancellationToken.Register(() => tcpClient.Dispose());
+            using (tcpClient)
+            {
+                try
+                {
+                    tcpClient.LingerState = new LingerOption(true, 5);
+                    using (var outClient = new TcpClient())
+                    {
+                        cancellationToken.Register(() => outClient.Dispose());
+                        outClient.LingerState = new LingerOption(true, 5);
+                        await outClient.ConnectAsync(_target, _port);
+
+                        var stream1 = tcpClient.GetStream();
+                        var stream2 = tcpClient.GetStream();
+                        var dir1 = CopyStream(true, stream1, stream2, cancellationToken);
+                        var dir2 = CopyStream(false, stream2, stream1, cancellationToken);
+                        await Task.WhenAny(dir1, dir2);
+
+                        outClient.Close();
+                    }
+                    tcpClient.Close();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private async Task CopyStream(bool outbound, NetworkStream inStream, NetworkStream outStream, CancellationToken cancellationToken)
+        {
+            DelayedSemaphore sync = _logger?.GetDelayedSemaphore();
+            byte[] buffer = new byte[65536];
+            while (true)
+            {
+                int bytesRead = await inStream.ReadAsync(buffer, 0, 65536, cancellationToken);
+                if (bytesRead == 0) return;
+                if (sync != null)
+                {
+                    await (sync.Wait(cancellationToken));
+                    try
+                    {
+                        await _logger.Log(_port, outbound, buffer, 0, bytesRead);
+                    }
+                    finally
+                    {
+                        sync.Release();
+                    }
+                }
+                await outStream.WriteAsync(buffer, 0, bytesRead, cancellationToken);
+            }
+        }
     }
 }
