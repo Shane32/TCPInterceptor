4436563874de07b90763b31eeb7b179f841628fe
Shane Krueger <shane@acdmail.com>
2019-05-05 14:41:54 -0400

    Updates

---
+++ TCPInterceptor/DelayedSemaphore.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPInterceptor
+{
+    class DelayedSemaphore
+    {
+        private readonly SemaphoreSlim _semaphoreSlim;
+        private bool _stillHeld = false;
+        private readonly Timer _timer;
+        private readonly int _timerDelay;
+        private readonly object _relaseSync = new object();
+
+        public DelayedSemaphore(SemaphoreSlim semaphoreSlim, int timerDelay)
+        {
+            _semaphoreSlim = semaphoreSlim;
+            _timer = new Timer(new TimerCallback(_timerCallback));
+            _timerDelay = timerDelay;
+        }
+
+        private void _timerCallback(object state)
+        {
+            lock (_relaseSync)
+            {
+                if (_stillHeld)
+                {
+                    _semaphoreSlim.Release();
+                    _stillHeld = false;
+                }
+            }
+        }
+
+        public Task Wait()
+        {
+            if (_stillHeld)
+            {
+                _stillHeld = false;
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                return Task.CompletedTask;
+            }
+            else
+            {
+                return _semaphoreSlim.WaitAsync();
+            }
+        }
+
+        public Task Wait(CancellationToken cancellationToken)
+        {
+            if (_stillHeld)
+            {
+                _stillHeld = false;
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                return Task.CompletedTask;
+            }
+            else
+            {
+                return _semaphoreSlim.WaitAsync(cancellationToken);
+            }
+        }
+
+        public void Release()
+        {
+            lock (_relaseSync)
+            {
+                if (!_stillHeld)
+                {
+                    _stillHeld = true;
+                    _timer.Change(_timerDelay, Timeout.Infinite);
+                }
+                else
+                {
+                    _semaphoreSlim.Release();
+                }
+            }
+        }
+    }
+}
---
+++ TCPInterceptor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPInterceptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var Program = new Program();
+                Program.Start(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        int[] ports;
+        IPAddress target;
+        int timeout;
+        string logFolderName;
+        System.Collections.Generic.Queue<int> queue;
+
+        void Start(string[] args)
+        {
+            Console.WriteLine("TCPInterceptor");
+            if (!GetParams()) return;
+            StartListeners();
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        bool GetParams()
+        {
+            Console.Write("Enter input ports separated by commas: ");
+            var str = Console.ReadLine();
+            if (str == "") return false;
+            ports = str.Split(',').Select(x => int.Parse(x)).ToArray();
+
+            Console.Write("Enter target IP address: ");
+            str = Console.ReadLine();
+            if (str == "") return false;
+            target = IPAddress.Parse(str);
+
+            Console.Write("Timeout sending along data: ");
+            str = Console.ReadLine();
+            if (str == "") return false;
+            timeout = int.Parse(str);
+
+            Console.Write("Log folder name: ");
+            str = Console.ReadLine();
+            if (str == "") return false;
+            logFolderName = str;
+
+            return true;
+        }
+
+    }
+}
